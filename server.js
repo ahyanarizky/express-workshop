c549987e553d7d118e6c46115a6ff4761096331d
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-10-28 21:27:50 +0700

    define books

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 
 //express dependencies
 const express = require('express');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 //initiate express
 const app = express()
-const router = app.router()
+const router = express.Router()
+
+// ---------------------------------------
+// APP CONFIGURATION
+//----------------------------------------
+
+//req.body-parser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+const books = require('./data.js');
+console.log(books);
