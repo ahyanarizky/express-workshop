9067ef95e3d6d85b56ca90161827d8a92fe70d2f
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-02 18:40:58 +0700

    add million dollar comment

--- server/controllers/api.books.js
+++ server/controllers/api.books.js
@@ -29,7 +29,19 @@ module.exports = {
             }
         })
     },
-    // GET api/books/id
+    // GET api/books/:id
+    /**
+ * @api {get} api/books/:id Request Single Book information
+ * @apiName GetBooksById
+ * @apiGroup Books
+ *
+ * @apiParam {Number} id Books unique ID.
+ *
+ * @apiSuccess {String} isbn ISBN number of the Book.
+ * @apiSuccess {String} name  Name of the Book.
+ * @apiSuccess {Number} price  Price of the Book.
+ */
+
     getBookById: (req, res) => {
 
         let book = books.filter(book => {
@@ -42,6 +54,20 @@ module.exports = {
         }
     },
     // POST api/books
+    /**
+ * @api {post} api/books/ Post new Book
+ * @apiName PostBook
+ * @apiGroup Books
+ *
+ * @apiParam {String} isbn ISBN number of the Book.
+ * @apiParam {String} name  Name of the Book.
+ * @apiParam {Number} price  Price of the Book.
+ *
+ * @apiSuccess {String} isbn ISBN number of the Book.
+ * @apiSuccess {String} name  Name of the Book.
+ * @apiSuccess {Number} price  Price of the Book.
+ */
+
     postBook: (req, res) => {
         Book.create({
             isbn: req.body.isbn,
@@ -57,6 +83,16 @@ module.exports = {
 
     },
     // DELETE api/books/:id
+    /**
+ * @api {delete} api/books/:id Delete Single Book
+ * @apiName DeleteBooksById
+ * @apiGroup Books
+ *
+ * @apiParam {Number} id Books unique ID.
+ *
+ * @apiSuccess book deleted from database
+ */
+
     deleteBook: (req, res) => {
         Book.remove({
             _id: req.params.id
@@ -71,6 +107,21 @@ module.exports = {
         })
     },
     // PUT api/books/:id
+    /**
+ * @api {put} api/books/:id Update a book
+ * @apiName UpdateBook
+ * @apiGroup Books
+ *
+ * @apiParam {Number} id Books unique ID.
+ * @apiParam {String} isbn ISBN number of the Book.
+ * @apiParam {String} name  Name of the Book.
+ * @apiParam {Number} price  Price of the Book.
+ *
+ * @apiSuccess {String} isbn ISBN number of the Book.
+ * @apiSuccess {String} name  Name of the Book.
+ * @apiSuccess {Number} price  Price of the Book.
+ */
+
     putBook: (req, res) => {
         Book.findOneAndUpdate({
             _id: req.params.id
