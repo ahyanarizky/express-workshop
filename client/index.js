621ae45fc1f0d83805b9a227df9382ca73f6f9b7
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-02 16:47:44 +0700

    edit client js

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //     console.log(data);
     //     $description.html(data[0].name)
     // })
-    $.getJSON(`http://localhost:3000/books`, function(data) {
+    $.getJSON(`http://localhost:3000/api/books`, function(data) {
         console.log(data);
         $description.html(`${data[0].id} : ${data[0].name} ${data[0].price}`)
     })
